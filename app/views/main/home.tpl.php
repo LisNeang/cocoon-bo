6641753150ad5f1362b4e78808c1ab70108a73e9
Lis neang <lisneang@gmail.com>
2020-09-15 10:14:14 +0200

    factorisation integration header - nav - home

--- app/views/main/home.tpl.php
+++ app/views/main/home.tpl.php
@@ -1,5 +1,164 @@
-<h1>Page d'accueil</h1>
-
-<p>
-  TODO
+<p class="display-4 message">
+      Bienvenue dans le backOffice
 </p>
+
+<div class="row mt-5">
+    <div class="col-12 col-md-6">
+        <div class="card text-white mb-3">
+            <div class="card-header">Liste des catégories</div>
+            <div class="card-body">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nom</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Industriel</td>
+                            <td class="text-right">
+                                <a href="" class="btn btn-sm btn-warning">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                </a>
+                                <!-- Example single danger button -->
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-danger dropdown-toggle"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                    </button>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">Oui, je veux supprimer</a>
+                                        <a class="dropdown-item" href="#" data-toggle="dropdown">Oups !</a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Ethnique</td>
+                            <td class="text-right">
+                                <a href="" class="btn btn-sm btn-warning">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                </a>
+                                <!-- Example single danger button -->
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-danger dropdown-toggle"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                    </button>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">Oui, je veux supprimer</a>
+                                        <a class="dropdown-item" href="#" data-toggle="dropdown">Oups !</a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Vintage Chic</td>
+                            <td class="text-right">
+                                <a href="" class="btn btn-sm btn-warning">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                </a>
+                                <!-- Example single danger button -->
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-danger dropdown-toggle"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                    </button>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">Oui, je veux supprimer</a>
+                                        <a class="dropdown-item" href="#" data-toggle="dropdown">Oups !</a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <a href="categories.html" class="btn btn-block btn-blue">Voir plus</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-md-6">
+        <div class="card text-white mb-3">
+            <div class="card-header">Liste des produits</div>
+            <div class="card-body">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nom</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Chaise</td>
+                            <td class="text-right">
+                                <a href="" class="btn btn-sm btn-warning">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                </a>
+                                <!-- Example single danger button -->
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-danger dropdown-toggle"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                    </button>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">Oui, je veux supprimer</a>
+                                        <a class="dropdown-item" href="#" data-toggle="dropdown">Oups !</a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Table</td>
+                            <td class="text-right">
+                                <a href="" class="btn btn-sm btn-warning">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                </a>
+                                <!-- Example single danger button -->
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-danger dropdown-toggle"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                    </button>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">Oui, je veux supprimer</a>
+                                        <a class="dropdown-item" href="#" data-toggle="dropdown">Oups !</a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Lampe</td>
+                            <td class="text-right">
+                                <a href="" class="btn btn-sm btn-warning">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                </a>
+                                <!-- Example single danger button -->
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-danger dropdown-toggle"
+                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                    </button>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">Oui, je veux supprimer</a>
+                                        <a class="dropdown-item" href="#" data-toggle="dropdown">Oups !</a>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <a href="products.html" class="btn btn-block btn-blue">Voir plus</a>
+            </div>
+        </div>
+    </div>
+</div>
